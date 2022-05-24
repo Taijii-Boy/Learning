85169462765bd5b2744d5333e93d06d715bba4b6
sorokinai <dao-prog@yandex.ru>
2022-05-24 10:05:25 +0300

    Учусь

--- Head First - Шаблоны проектирования/Команда/CommandPattern.py
+++ Head First - Шаблоны проектирования/Команда/CommandPattern.py
@@ -33,6 +33,12 @@ class RemoteControl:
     def __init__(self):
         self.on_commands = {}
         self.off_commands = {}
+        self.no_command = NoCommand()
+        self.undo_command = NoCommand()
+
+        for slot in range(7):
+            self.on_commands[slot] = self.no_command
+            self.off_commands[slot] = self.no_command
 
     def set_command(self, slot: int, on_command: Command, off_command: Command):
         self.on_commands[slot] = on_command
@@ -40,65 +46,57 @@ class RemoteControl:
 
     def on_button_was_pressed(self, slot: int):
         self.on_commands[slot].execute()
+        self.undo_command = self.on_commands[slot]
 
     def off_button_was_pressed(self, slot: int):
         self.off_commands[slot].execute()
+        self.undo_command = self.off_commands[slot]
+
+    def undo_button_was_pushed(self):
+        self.undo_command.undo()
 
     def to_string(self) -> list:
         string_buffer = []
         string_buffer.append('\n-------Remote Control -------')
+
         for slot in range(7):
-            try:
-                string_buffer.append(f'[slot {slot}] {self.on_commands[slot].name} -- {self.off_commands[slot].name}')
-            except:
-                string_buffer.append(f'[slot {slot}] Слот пустой')
-        string_buffer.append('-'*30)
+            on_command_text = f'[slot {slot}] {self.on_commands[slot].__class__.__name__} -- '
+            off_command_text = f'{self.off_commands[slot].__class__.__name__}'
+            string_buffer.append(on_command_text + off_command_text)
+
+        string_buffer.append(f'[undo] {self.undo_command.__class__.__name__}')
+        string_buffer.append('-' * 30)
         return string_buffer
 
 
 class RemoteLoader:
     def main(self):
-        remote = RemoteControl()
+        remote_control = RemoteControl()
         living_room_light = Light('Светильник в гостиной')
-        kitchen_light = Light('Светильник на кухне')
-        ceiling_fun = CeilingFan('Вентилятор в гостиной')
-        garage_door = GarageDoor()
-        stereo = Stereo('Стерео система в гостиной')
 
         living_room_light_on = LightOnCommand(living_room_light)
         living_room_light_off = LightOffCommand(living_room_light)
-        kitchen_light_on = LightOnCommand(kitchen_light)
-        kitchen_light_off = LightOffCommand(kitchen_light)
 
-        ceiling_fun_on = CeilingFanHighCommand(ceiling_fun)
-        ceiling_fun_off = CeilingFanOffCommand(ceiling_fun)
+        remote_control.set_command(0, living_room_light_on, living_room_light_off)
 
-        garage_door_up = GarageDoorOpenCommand(garage_door)
-        garage_door_down = GarageDoorCloseCommand(garage_door)
+        remote_control.on_button_was_pressed(0)
+        remote_control.off_button_was_pressed(0)
 
-        stereo_on_with_cd = StereoOnWithCDCommand(stereo)
-        stereo_off = StereoOffCommand(stereo)
+        self.print_slots_information(remote_control)
 
-        remote.set_command(0, living_room_light_on, living_room_light_off)
-        remote.set_command(1, kitchen_light_on, kitchen_light_off)
-        remote.set_command(2, ceiling_fun_on, ceiling_fun_off)
-        remote.set_command(3, garage_door_up, garage_door_down)
-        remote.set_command(4, stereo_on_with_cd, stereo_off)
+        remote_control.undo_button_was_pushed()
+        remote_control.off_button_was_pressed(0)
+        remote_control.on_button_was_pressed(0)
 
+        self.print_slots_information(remote_control)
+
+        remote_control.undo_button_was_pushed()
+
+    @staticmethod
+    def print_slots_information(remote):
         slots_information = remote.to_string()
         for slot in slots_information:
             print(slot)
-
-        remote.on_button_was_pressed(0)
-        remote.off_button_was_pressed(0)
-        remote.on_button_was_pressed(1)
-        remote.off_button_was_pressed(1)
-        remote.on_button_was_pressed(2)
-        remote.off_button_was_pressed(2)
-        remote.on_button_was_pressed(3)
-        remote.off_button_was_pressed(3)
-        remote.on_button_was_pressed(4)
-        remote.off_button_was_pressed(4)
 
 
 if __name__ == '__main__':
